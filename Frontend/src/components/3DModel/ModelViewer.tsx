c81ca44c11ee948b2a6ee9ea664d01de8d124b54
Farzam <89484118+FarzamN@users.noreply.github.com>
2025-12-30 14:49:08 +0500

    Update ModelViewer.tsx

--- Frontend/src/components/3DModel/ModelViewer.tsx
+++ Frontend/src/components/3DModel/ModelViewer.tsx
@@ -1,25 +1,25 @@
 import { Canvas } from "@react-three/fiber";
 import AutoFitModel from "./AutoFitModel";
 import type { UrlProps } from "@/lib/type";
-import { isIOS } from "@/lib/function";
+// import { isIOS } from "@/lib/function";
 import { OrbitControls } from "@react-three/drei";
 
 const ModelViewer = ({ glbUrl, usdzUrl }: UrlProps) => {
-  if (isIOS && usdzUrl) {
-    return (
-      <div className="flex flex-col items-center gap-3">
-        <p className="text-sm text-muted-foreground">View in AR</p>
+  // if (isIOS && usdzUrl) {
+  //   return (
+  //     <div className="flex flex-col items-center gap-3">
+  //       <p className="text-sm text-muted-foreground">View in AR</p>
 
-        <a
-          href={usdzUrl}
-          rel="ar"
-          className="px-4 py-2 rounded-md bg-black text-white"
-        >
-          Open AR
-        </a>
-      </div>
-    );
-  }
+  //       <a
+  //         href={usdzUrl}
+  //         rel="ar"
+  //         className="px-4 py-2 rounded-md bg-black text-white"
+  //       >
+  //         Open AR
+  //       </a>
+  //     </div>
+  //   );
+  // }
 
   return (
     <Canvas camera={{ position: [0, 1.5, 4], fov: 50 }}>
