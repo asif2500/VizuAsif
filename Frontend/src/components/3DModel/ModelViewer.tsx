437e9d089ec91475fc335ebdbe53fed35076312d
Farzam <89484118+FarzamN@users.noreply.github.com>
2025-12-30 16:31:06 +0500

    Update ModelViewer.tsx

--- Frontend/src/components/3DModel/ModelViewer.tsx
+++ Frontend/src/components/3DModel/ModelViewer.tsx
@@ -1,31 +1,30 @@
 import { Canvas } from "@react-three/fiber";
 import AutoFitModel from "./AutoFitModel";
 import type { UrlProps } from "@/lib/type";
-import { isIOS } from "@/lib/function";
+// import { isIOS } from "@/lib/function";
 import { OrbitControls } from "@react-three/drei";
 
 const ModelViewer = ({ glbUrl, usdzUrl }: UrlProps) => {
-
-if (isIOS && usdzUrl) {
-    return (
-      <div className="flex flex-col items-center gap-3">
-<a
-      href="../../assets/MER_static.usdz"
-      rel="ar"
-      style={{
-        display: "inline-block",
-        padding: "12px 20px",
-        background: "#000",
-        color: "#fff",
-        borderRadius: "8px",
-        textDecoration: "none"
-      }}
-    >
-      View in AR
-    </a>
-      </div>
-    );
-  }
+  // if (isIOS && usdzUrl) {
+  //   return (
+  //     <div className="flex flex-col items-center gap-3">
+  //       <a
+  //         href="../../assets/MER_static.usdz"
+  //         rel="ar"
+  //         style={{
+  //           display: "inline-block",
+  //           padding: "12px 20px",
+  //           background: "#000",
+  //           color: "#fff",
+  //           borderRadius: "8px",
+  //           textDecoration: "none",
+  //         }}
+  //       >
+  //         View in AR
+  //       </a>
+  //     </div>
+  //   );
+  // }
 
   return (
     <Canvas camera={{ position: [0, 1.5, 4], fov: 50 }}>
