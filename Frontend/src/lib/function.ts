b7c33fe1c61bd5c4a46952215a630059b36b6360
Farzam <89484118+FarzamN@users.noreply.github.com>
2025-12-30 16:34:35 +0500

    Update function.ts

--- Frontend/src/lib/function.ts
+++ Frontend/src/lib/function.ts
@@ -2,16 +2,21 @@ export const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
 export const isAndroid = /Android/.test(navigator.userAgent);
 
 export const launchAR = (glbUrl:string,usdzUrl:string) => {
-    if (isAndroid) {
-      const sceneViewerUrl = `https://arvr.google.com/scene-viewer/1.0?file=${encodeURIComponent(
-        glbUrl
-      )}&mode=ar_preferred`;
+  const sceneViewerUrl = `https://arvr.google.com/scene-viewer/1.0?file=${encodeURIComponent(
+    glbUrl
+  )}&mode=ar_preferred`;
 
-      window.location.href = sceneViewerUrl;
-    } 
-    else if (isIOS && usdzUrl) {
-      window.location.href = usdzUrl;
-    } 
+  window.location.href = sceneViewerUrl;
+    // if (isAndroid) {
+    //   const sceneViewerUrl = `https://arvr.google.com/scene-viewer/1.0?file=${encodeURIComponent(
+    //     glbUrl
+    //   )}&mode=ar_preferred`;
+
+    //   window.location.href = sceneViewerUrl;
+    // } 
+    // else if (isIOS && usdzUrl) {
+    //   window.location.href = usdzUrl;
+    // } 
     else {
       alert("AR not supported on this device");
     }
